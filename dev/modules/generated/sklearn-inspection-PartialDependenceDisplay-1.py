af6b73601ad9d1b05d5cf9f4880a5dce862a6f6f
scikit-learn <olivier.grisel+sklearn-ci@gmail.com>
2021-11-18 10:16:46 +0000

    Pushing the docs to dev/ for branch: main, commit f4de2d121a2a2f6d525301e3b60c66e05289b0fe

--- dev/modules/generated/sklearn-inspection-PartialDependenceDisplay-1.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from sklearn.datasets import make_friedman1
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.inspection import PartialDependenceDisplay
-X, y = make_friedman1()
-clf = GradientBoostingRegressor(n_estimators=10).fit(X, y)
-PartialDependenceDisplay.from_estimator(clf, X, [0, (0, 1)])
-# <...>
-plt.show()
